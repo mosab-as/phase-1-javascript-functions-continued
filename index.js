de56ac62da28205943541fc9626c80e1dc07eae2
Golden-Man <Golden-Man@users.noreply.github.com>
2022-10-22 02:32:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun (arg = 'roller-skate')
+{
+    return `This Saturday, I want to ${arg}!`
+}
+
+function mondayWork(arg = 'go to the office')
+{
+    return `This Monday, I will ${arg}.`
+}
+
+function wrapAdjective(arg = "*")
+{
+    return function(innerArg = "special"){
+        return `You are ${arg+innerArg+arg}!`
+    }
+}
